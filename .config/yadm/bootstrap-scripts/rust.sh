cbdeaaaddaa079dd96e2cbe9e6383f83e7153c12
Rickard Lindahl <mail@rickardlindahl.com>
2022-04-14 21:34:59 +0200

    Update rust bootstrap script

--- .config/yadm/bootstrap-scripts/rust.sh
+++ .config/yadm/bootstrap-scripts/rust.sh
@@ -1,7 +1,23 @@
 #!/usr/bin/env bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+. $SCRIPT_DIR/apt-install.sh
+
 install_rust() {
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+}
+
+install_requirements() {
+    packages=(
+        "libfontconfig1-dev"
+        "libxcb-render0-dev"
+        "libxcb-shape0-dev"
+        "libxcb-xfixes0-dev"
+    )
+    
+    for package in "${packages[@]}"; do
+        apt_install $package
+    done
 }
 
 install_cargo_packages() {
@@ -20,8 +36,11 @@ install_cargo_packages() {
 
 if [[ $(command -v rustup >/dev/null 2>&1) -ne 0 ]]; then
     install_rust
-    install_cargo_packages
 else
-    echo "Rust and cargo is already installed"
+    echo "Rustup is already installed"
 fi
+
+install_requirements
+install_cargo_packages
+
 
